f2be130bb37cecdce8b6d1f0f0941c89b33b4e5f
Blexander Bolander <alexander.bolander@elev.ga.ntig.se>
2025-01-29 14:57:31 +0100

    first commit

---
+++ Balexnaders Casino/Program.cs
@@ -0,0 +1,16 @@
+﻿//variables
+int money = 300;
+int betcurrent = 0;
+
+
+
+
+
+
+Console.WriteLine("HELLO AND WELCOME TO BALEXANDERS CASINO! THE BEST CASINO IN THE ENTIRE WORLD!!!!");
+Console.WriteLine("WE HAVE A BUNCH OF DIFFRENT GAMES LIKE SLOTS!! HORSEBETTING!! AND LAST BUT NOT LEAST PLINKO!!!!");
+Console.WriteLine($"You have [money] Euro");
+
+
+
+Console.ReadLine();
