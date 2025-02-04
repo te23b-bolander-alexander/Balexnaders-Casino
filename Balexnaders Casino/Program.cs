9c1deb618585248ca4f7d11a27dee90f865f8eff
Blexander Bolander <alexander.bolander@elev.ga.ntig.se>
2025-02-04 13:25:57 +0100

    sbibibibi

--- Balexnaders Casino/Program.cs
+++ Balexnaders Casino/Program.cs
@@ -1,15 +1,18 @@
-﻿//variables
-int money = 300;
-int betcurrent = 0;
-
-
-
-
-
+﻿//Strings 
+string money = "300";
+string slots = ("");
+string horsebetting = ("");
+string plinko = ("");
 
 Console.WriteLine("HELLO AND WELCOME TO BALEXANDERS CASINO! THE BEST CASINO IN THE ENTIRE WORLD!!!!");
 Console.WriteLine("WE HAVE A BUNCH OF DIFFRENT GAMES LIKE SLOTS!! HORSEBETTING!! AND LAST BUT NOT LEAST PLINKO!!!!");
 Console.WriteLine($"You have [money] Euro");
+Console.WriteLine("What game would you like to play?");
+
+while ()
+{
+
+}
 
 
 
--- Balexnaders Casino/obj/Debug/net8.0/Balexnaders Casino.AssemblyInfo.cs
+++ Balexnaders Casino/obj/Debug/net8.0/Balexnaders Casino.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Balexnaders Casino")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17b19687b19525c323600dbd88282d0622c69145")]
 [assembly: System.Reflection.AssemblyProductAttribute("Balexnaders Casino")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Balexnaders Casino")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
