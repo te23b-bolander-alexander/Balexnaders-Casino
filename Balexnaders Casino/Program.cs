a12f9469634846b18c371b07a3f5065d3301c52b
Blexander Bolander <alexander.bolander@elev.ga.ntig.se>
2025-02-05 15:00:39 +0100

    skibidi (hade ingen kompis att slösa tid med idag :( )

--- Balexnaders Casino/Program.cs
+++ Balexnaders Casino/Program.cs
@@ -1,4 +1,4 @@
-﻿//Strings 
+﻿ 
 string money = "300";
 string slots = ("");
 string horsebetting = ("");
@@ -6,13 +6,21 @@ string plinko = ("");
 
 Console.WriteLine("HELLO AND WELCOME TO BALEXANDERS CASINO! THE BEST CASINO IN THE ENTIRE WORLD!!!!");
 Console.WriteLine("WE HAVE A BUNCH OF DIFFRENT GAMES LIKE SLOTS!! HORSEBETTING!! AND LAST BUT NOT LEAST PLINKO!!!!");
-Console.WriteLine($"You have [money] Euro");
+Console.WriteLine($"You have {money} Euro");
 Console.WriteLine("What game would you like to play?");
+Console.WriteLine("slots, horsebetting or plinko?");
 
-while ()
+
+//Checker that checks if you have chosen a game or not.
+string right = Console.ReadLine();
+int rightnum = 0;
+bool success = int.TryParse(right, out rightnum);
+if (success == false)
 {
-
+    Console.WriteLine("You have to select a game");
 }
+
+
 
 
 
