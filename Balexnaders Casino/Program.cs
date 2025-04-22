154176064fb1e110f416ab666f918d3860a5c82c
Blexander Bolander <alexander.bolander@elev.ga.ntig.se>
2025-04-22 12:18:12 +0200

    KOMM

--- Balexnaders Casino/Program.cs
+++ Balexnaders Casino/Program.cs
@@ -1,25 +1,70 @@
-﻿ 
-string money = "300";
-string slots = ("");
-string horsebetting = ("");
-string plinko = ("");
-
-Console.WriteLine("HELLO AND WELCOME TO BALEXANDERS CASINO! THE BEST CASINO IN THE ENTIRE WORLD!!!!");
-Console.WriteLine("WE HAVE A BUNCH OF DIFFRENT GAMES LIKE SLOTS!! HORSEBETTING!! AND LAST BUT NOT LEAST PLINKO!!!!");
-Console.WriteLine($"You have {money} Euro");
-Console.WriteLine("What game would you like to play?");
-Console.WriteLine("slots, horsebetting or plinko?");
+﻿
+using System.Net.WebSockets;
+using System.Xml.Serialization;
 
 
-//Checker that checks if you have chosen a game or not.
-string right = Console.ReadLine();
-int rightnum = 0;
-bool success = int.TryParse(right, out rightnum);
-if (success == false)
+// A bunch of ints and a random damage generator
+Random rnd = new Random();
+int AlphaHp = 40;
+int SigmaHp = 40;
+int AlphaAttack = rnd.Next(1, 10);
+int SigmaAttack = rnd.Next(1, 10);
+int AlphaHeal = rnd.Next(1, 3);
+int SigmaHeal = rnd.Next(1, 5);
+
+
+
+//everything that the alpha can do :)
+while(AlphaHp > 0 && SigmaHp > 0)
 {
-    Console.WriteLine("You have to select a game");
+
+//The Alphas turn
+Console.WriteLine(" Its time for the Alpha to attack ");
+Console.WriteLine($"p-p Alphas Hp is {AlphaHp} p-p The Sigmas Hp is {SigmaHp}");
+Console.WriteLine(" press 'a' to attack the sigma or 'h' to heal yourself (the alpha)");
+
+string Alphachoose = Console.ReadLine();
+
+if (Alphachoose == "a")
+{
+    SigmaHp -= AlphaAttack;
+    Console.WriteLine($"The alpha attacked the sigma and did {AlphaAttack} Damage!!! Wowzers");
+}
+else 
+{
+    AlphaHp += AlphaHeal;
+    Console.WriteLine($"the alpha choose to heal for {AlphaHeal} amount of health");
 }
 
+
+// everything the sigma can do
+if(SigmaHp > 0)
+{
+    Console.WriteLine("|| huh i guess tis time for the sigma to attack ||");
+    Console.WriteLine($"p-p Alphas Hp is {AlphaHp} p-p The Sigmas Hp is {SigmaHp}");
+    int SigmaChoose = rnd.Next(0, 2);
+
+    if(SigmaChoose == 0)
+    {
+        AlphaHp -= SigmaAttack;
+        Console.WriteLine($"|| THe sigma choose to attack the alpha and did {SigmaAttack} Damage ||");
+    }
+    else
+    {
+        SigmaHp += SigmaHeal;
+        Console.WriteLine($"|| The sigma choose to heal for {SigmaHeal} health||");
+    }
+
+    if(AlphaHp > 0)
+    {
+        Console.WriteLine("Huh the alpha stays victorious what a suprise  : o");
+    }
+    else
+    {
+        Console.WriteLine("Seems like the Sigma won i mean sigma and smegma are kinda close so it makes sense i guess");//wtf /alex.s
+    }
+}
+}
 
 
 
--- Balexnaders Casino/obj/Debug/net8.0/Balexnaders Casino.AssemblyInfo.cs
+++ Balexnaders Casino/obj/Debug/net8.0/Balexnaders Casino.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Balexnaders Casino")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c1deb618585248ca4f7d11a27dee90f865f8eff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a12f9469634846b18c371b07a3f5065d3301c52b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Balexnaders Casino")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Balexnaders Casino")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
